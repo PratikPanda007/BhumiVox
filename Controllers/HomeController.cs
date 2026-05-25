c859bb1462daec5d4f0250f7f1f43f4df5cd8c4b
PratikPanda <pratic.panda@gmail.com>
2026-05-25 22:53:42 +0530

    Added Base Code for BhumiVox

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BhumiVox.Models;
+
+namespace BhumiVox.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        // GET: api/home
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello from HomeController (GET)",
+                serverTime = DateTime.UtcNow
+            });
+        }
+
+        // POST: api/home
+        [HttpPost]
+        public IActionResult Post([FromBody] HomeRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(new
+            {
+                message = "Data received successfully (POST)",
+                receivedData = request
+            });
+        }
+    }
+}
---
+++ Models/HomeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BhumiVox.Models
+{
+    public class HomeRequest
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
