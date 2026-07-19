c630644f463e286c3aea446849bfe697ba9eedd2
PratikPanda <pratic.panda@gmail.com>
2026-07-20 01:08:27 +0530

    Itegrating Documents related flow, not completed.

---
+++ Models/Admin/UploadDocumentRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BhumiVox.Models.Admin
+{
+    public class UploadDocumentRequest
+    {
+        public int DocumentTypeId { get; set; }
+        public IFormFile File { get; set; } = null!;
+    }
+}
